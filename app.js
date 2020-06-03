2b6052eb7165c5cc1d46ef415f9444c4ea6a2986
joaquin98gv <59378355+joaquin98gv@users.noreply.github.com>
2020-06-03 16:28:07 -0400

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 // Inicializar variables
 var app = express();
+
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 // Conexion a la db
@@ -15,12 +27,9 @@ mongoose.connection.openUri('mongodb://Localhost:27017/hospitalDB', (err, res) =
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
